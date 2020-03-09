d23772aed4f62fe5db2b299561b48152985720e3
Tom Wire <tom.w.wire@gmail.com>
2020-03-09 21:26:09 +0000

    Add some required fields to prove validation works

--- src/UserApi/Dto/UserDto.cs
+++ src/UserApi/Dto/UserDto.cs
@@ -8,12 +8,15 @@ namespace UserApi.Dto
         public int Id { get; set; }
 
         [MaxLength(255)]
+        [Required]
         public string Email { get; set; }
 
         [MaxLength(255)]
+        [Required]
         public string GivenName { get; set; }
 
         [MaxLength(255)]
+        [Required]
         public string FamilyName { get; set; }
 
         public DateTime Created { get; set; }
--- src/UserApi/Models/User.cs
+++ src/UserApi/Models/User.cs
@@ -9,12 +9,15 @@ namespace UserApi.Models
         public int Id { get; set; }
 
         [MaxLength(255)]
+        [Required]
         public string Email { get; set; }
 
         [MaxLength(255)]
+        [Required]
         public string GivenName { get; set; }
 
         [MaxLength(255)]
+        [Required]
         public string FamilyName { get; set; }
 
         public DateTime Created { get; set; }
