ad1e73bbe2e954910e4a79605728180a375d9ed9
Tom Wire <tom.w.wire@gmail.com>
2020-03-09 18:44:00 +0000

    Initial commit

---
+++ tests/UserApi.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace UserApi.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
