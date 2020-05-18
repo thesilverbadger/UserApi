38487d981092415b6ff061580d43ebfc4e850c44
Tom Wire <tom.w.wire@gmail.com>
2020-05-18 20:52:10 +0100

    Add Authorize to the user controller

--- src/UserApi/Api/UsersController.cs
+++ src/UserApi/Api/UsersController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UserApi.Dto;
@@ -11,6 +12,7 @@ namespace UserApi.Api
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class UsersController : ControllerBase
     {
         private readonly IUserRepository _userRepository;
