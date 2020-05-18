b378646854db60c538796324be2cb39a711fcba7
Tom Wire <tom.w.wire@gmail.com>
2020-05-18 20:51:56 +0100

    Hook up authentication.authorisation

--- src/UserApi/Startup.cs
+++ src/UserApi/Startup.cs
@@ -13,6 +13,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using UserApi.Repositories;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace UserApi
 {
@@ -42,7 +45,27 @@ namespace UserApi
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "User API", Version = "v1" });
             });
 
+            AddOAuth(services);
+
             _services = services;
+        }
+
+        private void AddOAuth(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +80,7 @@ namespace UserApi
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseSwagger();
